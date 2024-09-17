a4f201137c63edb1f808b7e33851a793783dfddd
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-09-17 15:32:04 +0300

    Modified concert.db to have data entries

--- assignment/schema.py
+++ assignment/schema.py
@@ -5,27 +5,29 @@ CURSOR = CONN.cursor()
 
 # Creating bands table
 CURSOR.execute('''
-CREATE TABLE IF NOT EXISTS bands (
+CREATE TABLE IF NOT EXISTS band (
+    id INTEGER PRIMARY KEY,
     name TEXT PRIMARY KEY,
     hometown TEXT)
 ''')
 
 # Creating venues table
 CURSOR.execute ('''
-CREATE TABLE IF NOT EXISTS venues (
+CREATE TABLE IF NOT EXISTS venue (
+    id INTEGER PRIMARY KEY,
     title TEXT PRIMARY KEY,
     city TEXT)
 ''')
 
 # Creating concerts table to merge with data from bands and venues table
 CURSOR.execute('''
-CREATE TABLE IF NOT EXISTS concerts (
+CREATE TABLE IF NOT EXISTS concert (
     id INTEGER PRIMARY KEY,
     band_name TEXT,
     venue_title TEXT,
     date TEXT,
-    FOREIGN KEY (band_name) REFERENCES bands(name),
-    FOREIGN KEY (venue_title) REFERENCES venues(title))
+    FOREIGN KEY (band_name) REFERENCES band(name),
+    FOREIGN KEY (venue_title) REFERENCES venue(title))
 ''')
 
 CONN.commit()
