f7c0994fa9cbb32197dcad8178ad5e76fc6fe0d0
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-09-18 21:54:04 +0300

    Modified band.py to define class Concert and Venue

--- assignment/concert.py
+++ assignment/concert.py
@@ -43,3 +43,16 @@ class Concert:
         """, (self.id,))
         band_name, band_hometown, venue_city = CURSOR.fetchone()
         return f"Hello {venue_city}!!!!! We are {band_name} and we're from {band_hometown}"
+    
+# Defining classes passed
+class Band:
+     def __init__(self, id, name, hometown):
+        self.id = id
+        self.name = name
+        self.hometown = hometown
+
+class Venue:
+       def __init__(self, id, title, city):
+        self.id = id
+        self.title = title
+        self.city = city
